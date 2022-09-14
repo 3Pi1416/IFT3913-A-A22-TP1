2f11bf58c4a39cd1e1eef9c7e34ec559beef392f
Hugo <hugo.carrier@umontreal.com>
2022-09-14 18:36:25 -0400

    Finish the main
    Main is waiting for the implantation of all the homework inside the dict_function

--- src/main.py
+++ src/main.py
@@ -1,15 +1,27 @@
 import sys
-import os
+
+
+def quit_program(args):
+    quit()
+
+#define all the function to that can be execute
+dict_function = {"EXIT": quit_program}
 
 
 def main():
     for line in sys.stdin:
-        if line.upper() == "EXIT\n":
-            quit()
-        else:
-            print(line)
+        command, args = read_line(line)
+        function_to_execute = dict_function.get(command.upper())
+        if(function_to_execute != None):
+            function_to_execute(args)
+        else :
+            print(f"{command} is not a valid command.")
+
+
+def read_line(line: str):
+    commands = line.strip().split(" ")
+    return commands.pop(0), commands
 
 
 if __name__ == "__main__":
     main()
-
