c6368c7cd4b4ce770cf5b5417fd8c28ddb7eaf14
Hugo <hugo.carrier@umontreal.com>
2022-09-15 00:41:28 -0400

    Start working on the second part of the homework: nvloc

--- src/main.py
+++ src/main.py
@@ -1,20 +1,22 @@
 import sys
+import nvloc
 
 
 def quit_program(args):
     quit()
 
-#define all the function to that can be execute
-dict_function = {"EXIT": quit_program}
+
+# define all the function to that can be execute
+dict_function = {"EXIT": quit_program, "NVLOC": nvloc_command}
 
 
 def main():
     for line in sys.stdin:
         command, args = read_line(line)
         function_to_execute = dict_function.get(command.upper())
-        if(function_to_execute != None):
+        if (function_to_execute != None):
             function_to_execute(args)
-        else :
+        else:
             print(f"{command} is not a valid command.")
 
 
